6886a1ea6affd55ab494c734299015346f21c49c
karenaiwazian <112626984+karenaiwazian@users.noreply.github.com>
2024-07-24 12:51:56 +0400

    Update app.js

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ document.querySelectorAll("button[data-modal]").forEach(button => {
 
 let tg = window.Telegram.WebApp
 tg.MainButton.show()
-tg.MainButton.onClick(() => {
-    openInvoice("https://t.me/$QzNbe_sZCUkiCgAAzw2jXh8XiKU")
-})
+tg.MainButton.onClick(openInvoice("https://t.me/$QzNbe_sZCUkiCgAAzw2jXh8XiKU"))
+Telegram.WebApp.onEvent('mainButtonClicked', openInvoice("https://t.me/$QzNbe_sZCUkiCgAAzw2jXh8XiKU"))
+    
